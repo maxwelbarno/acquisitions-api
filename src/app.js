33e8b75e98b2cd8edcd3a09b14d11830fc580449
Maxwel Barno <barxwells@gmail.com>
2025-10-05 11:14:33 +0300

    build: set up logging and  middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.status(200).send('Hello from Acquisitions'));
+app.use(helmet());
+app.use(cors());
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  logger.info('Hello from Acquisition');
+  res.status(200).send('Hello from Acquisitions');
+});
 
 export default app;
